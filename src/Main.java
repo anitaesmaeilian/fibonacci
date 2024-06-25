611e43ce52c774645c5b26e697fffe61a2c3f942
anitaesmaeilian <anitaesmaeilian@gmail.com>
2024-06-25 18:28:05 +0330

    prints the difference of the numbers of the previous line until there is no more numbers left

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@ public class Main {
         int[][] fibo = new int[n][];
         for (int i=0,j=n;i<n;i++,j--)
             fibo[i] = new int[j];
+
+        // prints the first n fibonacci numbers
         for(int i=0 ; i<n ; i++)
         {
             fibo [0][i] = prepre;
@@ -16,6 +18,25 @@ public class Main {
             int next=pre+prepre;
             prepre=pre;
             pre=next;
+        }
+
+        // prints the difference of the numbers of the previous line until there is no more numbers left
+        System.out.println();
+        int z=n;
+        for (int j = 0 ; j<=z ; j++)
+        {
+            for (int i = 0; i < n-1; i++)
+            {
+                int difference;
+                if (fibo[j][i] > fibo[j][i+1])
+                    difference = fibo[j][i] - fibo[j][i+1];
+                else
+                    difference = fibo[j][i+1] - fibo[j][i];
+                fibo[j+1][i] = difference;
+                System.out.printf("%d ", fibo[j+1][i]);
+            }
+            n--;
+            System.out.println();
         }
     }
 }
